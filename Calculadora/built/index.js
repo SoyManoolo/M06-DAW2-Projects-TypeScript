eb7f5ed62dc804e233e17a6e1865c99a039984a4
SoyManoolo <erik.saldi.diaz@gmail.com>
2024-10-09 19:31:29 +0200

    feat: Added a project of a calculator.

---
+++ Calculadora/built/index.js
@@ -0,0 +1,47 @@
+"use strict";
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+let delAll = false;
+if (display) {
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            let cadena = display.value;
+            let ultimocaracter = cadena[cadena.length - 1];
+            if (btn.id == "%") {
+                let numerotext = cadena.substring(0, cadena.length);
+                let numero = +numerotext / 100;
+                display.value = numero.toString();
+                delAll = true;
+            }
+            else if (btn.id === "=") {
+                try {
+                    display.value = eval(display.value);
+                    delAll = true;
+                }
+                catch (error) {
+                    display.value = "Error";
+                    delAll = true;
+                }
+            }
+            else if (btn.id === "ac") {
+                display.value = "";
+                delAll = false;
+            }
+            else if (btn.id === "del") {
+                if (delAll) {
+                    display.value = "";
+                    delAll = false;
+                }
+                else {
+                    display.value = display.value.slice(0, -1);
+                }
+            }
+            else if (delAll) {
+                display.value = btn.id;
+            }
+            else {
+                display.value += btn.id;
+            }
+        });
+    });
+}
---
+++ Calculadora/src/index.ts
@@ -0,0 +1,43 @@
+const display = document.querySelector<HTMLInputElement>("#display")!;
+const buttons = document.querySelectorAll<HTMLButtonElement>("button");
+
+let delAll: boolean = false;
+
+if (display) {
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+
+            let cadena: string = display.value;
+            let ultimocaracter: string = cadena[cadena.length -1];
+
+            if (btn.id == "%") {
+                let numerotext: string = cadena.substring(0, cadena.length);
+                let numero: number = +numerotext / 100;
+                display.value = numero.toString();
+                delAll = true;
+            } else if (btn.id == "=") {
+                try {
+                    display.value = eval(display.value); 
+                    delAll = true;
+                } catch (error) {
+                    display.value = "Error";
+                    delAll = true;
+                }
+            } else if (btn.id == "ac") {
+                display.value = "";
+                delAll = false;
+            } else if (btn.id == "del") {
+                if(delAll) {
+                    display.value = "";
+                    delAll = false;
+                } else {
+                    display.value = display.value.slice(0, -1);
+                }
+            }else if(delAll){
+                display.value = btn.id;
+            } else {
+                display.value += btn.id
+            }
+        })
+    })
+}
