e203ffb4a0381d06d6f9f4d331c00f4a4ac72485
SoyManoolo <erik.saldi.diaz@gmail.com>
2024-10-09 20:39:16 +0200

    feat: function: the button change the background

---
+++ Color_picker/built/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function changeBackground() {
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "#ff2d00";
+}
---
+++ Color_picker/src/ts/index.ts
@@ -0,0 +1,4 @@
+function changeBackground() {
+    let body: HTMLElement = document.getElementById("body")!;
+    body.style.backgroundColor= "#ff2d00"
+}
