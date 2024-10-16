3762b8a08f083842dfed7fc610ed9d9658912ce7
SoyManoolo <erik.saldi.diaz@gmail.com>
2024-10-16 20:33:53 +0200

    feat: Add TypeScript code for Exercise 1 and Exercise 2
    - Add TypeScript code for Exercise 1, including variable declarations and console logs
    - Add TypeScript code for Exercise 2, including constant declarations
    
    Closes: #123

---
+++ Practica2/Exercici1/src/ts/index.ts
@@ -0,0 +1,17 @@
+var var_ErikSaldana: number = 30; // Tipus de dada number i amb valor 30
+console.log(var_ErikSaldana); // Mostra el valor de la variable per consola
+
+var_ErikSaldana = 10; // Canvia el valor de la variable a 10
+console.log(var_ErikSaldana); // Mostra el valor de la variable per consola
+
+let let_ErikSaldana: string = "Hola món!"; // Tipus de dada string i amb valor Hola món!
+console.log(let_ErikSaldana); // Mostra el valor de la variable per consola
+
+let_ErikSaldana = "Adeu món!"; // Canvia el valor de la variable a Adeu món!
+console.log(let_ErikSaldana); // Mostra el valor de la variable per consola
+
+const const_ErikSaldana: string = "Javascript obsolet"; // Tipus de dada string i amb valor Javascript obsolet
+console.log(const_ErikSaldana); // Mostra el valor de la variable per consola
+
+// const_ErikSaldana = "Typescript"; // Error, no es pot canviar el valor de una constant el VSC em recomana canviar la variable a let
+
---
+++ Practica2/Exercici2/src/ts/index.ts
@@ -0,0 +1,4 @@
+const visitesMaximes: number = 2; // Declaro visitesMaximes com a const per a que no es pugui canviar 
+let comptadorVisites: number; // Declaro comptadorVisites com a let perque ha de canviar cada vegada que una persona es a dins
+const nomPagina: string = "Hola K Ace"; // Declaro nomPagina com a const, ja que el nom de la página no canviará
+const missatgeBenvinguda: string = "Bienvenido";
