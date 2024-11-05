a9e4de67e7e0bf11b0c2335869447276eca7d348
SoyManoolo <erik.saldi.diaz@gmail.com>
2024-11-05 12:22:28 +0100

    feat: Update input label and button functionality; implement word processing logic

--- Practica3/Exercici3/built/index.js
+++ Practica3/Exercici3/built/index.js
@@ -1,19 +1,20 @@
+"use strict";
 function paraula() {
-    let word: string = (document.getElementById("paraula") as HTMLInputElement).value;
-
-    let letras: string[] = word.split("");
-    let cantLetras: number = letras.length;
-
+    let word = document.getElementById("paraula").value;
+    let letras = word.split("");
+    let cantLetras = letras.length;
     if (cantLetras % 4 == 0) {
         console.log("La paraula es divisible per 4");
-    } else {
+    }
+    else {
         console.log("La paraula no es divisible per 4");
     }
-
     for (let i = 0; i < cantLetras; i++) {
         if (i % 2 == 0) {
             console.log(letras[i].toUpperCase());
-        } else {
-            console.log(letras[i].toLowerCase());}
+        }
+        else {
+            console.log(letras[i].toLowerCase());
+        }
     }
 }
