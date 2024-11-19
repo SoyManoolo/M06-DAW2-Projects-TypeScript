57f0a861a4b40dcbeb1028276d4c0c9ab980ef88
SoyManoolo <erik.saldi.diaz@gmail.com>
2024-11-19 11:45:15 +0100

    feat: Refactor category and product management; implement dynamic addition and display of products and categories

--- UF2/Exercici4/built/index.js
+++ UF2/Exercici4/built/index.js
@@ -1,11 +1,49 @@
 "use strict";
-let categories = new Set();
-const div = document.getElementById("answer");
-function addCategory() {
-    let category = document.getElementById("category").value;
-    categories.add(category);
-    console.log(category + " se ha añadido correctamente");
+let categories = new Map();
+let productes = new Map();
+function addTo() {
+    const btnId = (event === null || event === void 0 ? void 0 : event.target).id;
+    if (btnId == "addCategory") {
+        let category = document.getElementById("category").value;
+        categories.set(category, category);
+        console.log(category + " se ha añadido correctamente");
+    }
+    else if (btnId == "addProduct") {
+        let product = document.getElementById("product").value;
+        let quant = parseInt(document.getElementById("cantidad").value);
+        productes.set(product, quant);
+        console.log(product + " " + quant + " se ha añadido correctamente");
+    }
 }
-function showCategories() {
-    div.innerHTML = [...categories].join("<br>");
+function show() {
+    const btnId = (event === null || event === void 0 ? void 0 : event.target).id;
+    if (btnId == "showCategories") {
+        const divCategories = document.getElementById("listaCategorias");
+        divCategories.innerHTML = "";
+        categories.forEach((value, key) => {
+            divCategories.innerHTML += value + "<br>";
+        });
+    }
+    else if (btnId == "showProducts") {
+        const divProductes = document.getElementById("listaProductos");
+        divProductes.innerHTML = "";
+        productes.forEach((value, key) => {
+            divProductes.innerHTML += key + " " + value + "<br>";
+        });
+    }
+}
+function sale() {
+    const product = document.getElementById("product").value;
+    const price = parseInt(document.getElementById("price").value);
+    productes.forEach((value, key) => {
+        if (key == product || value > 0) {
+            productes.set(key, value - 1);
+            if (value == 0) {
+                productes.delete(key);
+            }
+        }
+        else {
+            alert("El producto no existe o el precio es incorrecto");
+        }
+    });
 }
--- UF2/Exercici4/src/ts/index.ts
+++ UF2/Exercici4/src/ts/index.ts
@@ -1,15 +1,51 @@
 let categories: Map<string, string> = new Map();
-const divCategories:HTMLElement = document.getElementById("lista-categorias")!;
+let productes: Map<string, number> = new Map();
 
-function addCategory() {
-    let category: string = (document.getElementById("category")! as HTMLInputElement).value;
-    categories.set("categoria", category);
-    console.log(category + " se ha añadido correctamente");
+
+function addTo() {
+    const btnId: string = (event?.target as HTMLElement).id;
+    
+    if(btnId == "addCategory") {
+        let category: string = (document.getElementById("category")! as HTMLInputElement).value;
+        categories.set(category, category);
+        console.log(category + " se ha añadido correctamente");
+    } else if(btnId == "addProduct") {
+        let product: string = (document.getElementById("product")! as HTMLInputElement).value;
+        let quant: number = parseInt((document.getElementById("cantidad")! as HTMLInputElement).value);
+        productes.set(product, quant);
+        console.log(product + " " + quant + " se ha añadido correctamente");
+}
 }
 
-function showCategories() {
-    categories.forEach((key, value) => {
-        divCategories.innerHTML += key + "<br>";
+function show() {
+    const btnId: string = (event?.target as HTMLElement).id;
+    if(btnId == "showCategories") {
+        const divCategories:HTMLElement = document.getElementById("listaCategorias")!;
+        divCategories.innerHTML = "";
+        categories.forEach((value, key) => {
+            divCategories.innerHTML += value + "<br>";
+        });
+    } else if(btnId == "showProducts") {
+        const divProductes:HTMLElement = document.getElementById("listaProductos")!;
+        divProductes.innerHTML = "";
+        productes.forEach((value, key) => {
+            divProductes.innerHTML += key + " " + value + "<br>";
+        });
+    }
+}
+
+function sale() {
+    const product: string = (document.getElementById("product")! as HTMLInputElement).value;
+    const price: number = parseInt((document.getElementById("price")! as HTMLInputElement).value);
+
+    productes.forEach((value, key) => {
+        if(key == product || value > 0) {
+            productes.set(key, value - 1);
+            if(value == 0) {
+                productes.delete(key);
+            }
+        } else {
+            alert("El producto no existe o el precio es incorrecto");
+        }
     });
-    divCategories.innerHTML = [...categories].join("<br>");
 }
